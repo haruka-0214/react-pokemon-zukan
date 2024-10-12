9010bfb4f3656abf586d4d6d37739d59b71499f7
ktc24a31e0015@edu.kyoto-tech.ac.jp <haruka0214>
2024-10-12 15:30:21 +0900

    きょうのコミット

---
+++ src/api/common.type.ts
@@ -0,0 +1,15 @@
+export interface NamedAPIResource {
+    name: string;
+    url: string;
+  }
+  
+  export interface Name {
+    name: string;
+    language: NamedAPIResource;
+  }
+  
+  export interface APIResource {
+    url: string;
+  }
+  ​
+  
